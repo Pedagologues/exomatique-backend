3f81b029aed3390264d268a090b624a16f0b173f
Wyrdix <wyrdix@gmail.com>
2024-05-01 21:41:06 +0200

    Remove CORS whitelisting

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,7 @@ const app: Express = express();
 var whitelist = ['http://localhost:3000']
 var corsOptions : CorsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin as string) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+    callback(null, true)
   }
 }
 
